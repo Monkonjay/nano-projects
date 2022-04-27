c58f7ad32c1b71bd4ebeba1743b1a8b26e5a014c
R Greene <greenbert81@gmail.com>
2022-04-27 00:28:08 -0700

    added valid input function

--- adventure_game.py
+++ adventure_game.py
@@ -11,13 +11,23 @@ def pause_print(message):
     time.sleep(2)
 
 
-# vairable to randomly select opponent
-opponent = random.choice(["Invincible Emogo", "Kakakolo, The Merciless,",
-                          "Jumpie The Destroyer,"])
+def valid_input(prompt, choice_a, choice_b):
+    while True:
+        choice = input(prompt).lower()
+        if choice_a in choice:
+            break
+        elif choice_b in choice:
+            break
+        else:
+            pause_print("Sorry, I don't understand.")
+    return choice
 
 
 # function to print introductory message
 def intro():
+    global opponent
+    opponent = random.choice (["Invincible Emogo", "Kakakolo, The Merciless,",
+                              "Jumpie The Destroyer,"])
     pause_print("You find yourself standing in an open field, "
                 "filled with grass and yellow wildflowers.")
     pause_print(f"Rumor has it that {opponent} is somewhere"
@@ -66,17 +76,17 @@ def starting_point():
     global been_inside_cave
     pause_print("Enter 1 to knock on the door of the house.")
     pause_print("Enter 2 to peer into the cave.")
-    house_or_cave = input("What would you like to do?\n(Please enter 1 or 2.)")
-    if house_or_cave == "1":
+    choice = valid_input("What would you like to do?\n(Please enter 1 or 2.)", "1", "2")
+    if choice == "1":
         house()
-    elif house_or_cave == "2":
+    elif choice == "2":
         if been_inside_cave is True:
             cave_story1()
         else:
             cave_story2()
-    else:
-        pause_print("Invalid Entry!")
-        starting_point()
+    # else:
+    #     pause_print("Invalid Entry!")
+     # starting_point()
 
 
 # battle outcome
@@ -87,16 +97,16 @@ def fight():
                     f"you unsheath your new sword.")
         pause_print("The Sword of Ogoroth shines brightly in "
                     "your hand as you brace yourself for the attack.")
-        pause_print("But the troll takes one look "
+        pause_print(f"But {opponent} takes one look "
                     "at your shiny new toy and runs away!")
         pause_print(f"You have rid the town of {opponent} . "
                     f"You are victorious!")
-        restart_inquiry()
+        restart()
     else:
         pause_print("You do your best...")
         pause_print(f"but your dagger is no match for {opponent}.")
         pause_print("You have been defeated!")
-        restart_inquiry()
+        restart()
 
 
 # option to decline to fight
@@ -107,29 +117,29 @@ def run():
 
 
 def battle_or_retreat():
-    fight_or_run = input("Do you want to fight (F) or run away (R)?").lower()
-    if "f" in fight_or_run:
+    choice = valid_input("Do you want to fight (F) or run away (R)?", "f", "r")
+    if choice == "f":
         fight()
-    elif "r" in fight_or_run:
+    elif choice == "r":
         run()
-    else:
-        pause_print("Invalid selection! Choose F to fight or R to run")
-        battle_or_retreat()
+    # else:
+    #     pause_print("Invalid selection! Choose F to fight or R to run")
+        # battle_or_retreat()
 
 
 # prompt to reply or end play
-def restart_inquiry():
-    new_game = input("Would you like to play again? (y/n)\n").lower()
-    if "y" in new_game:
+def restart():
+    choice = valid_input("Would you like to play again? (y/n)", "y", "n")
+    if choice == "y":
         pause_print("Hold on..restarting game")
         intro()
         starting_point()
-    elif "n" in new_game:
+    elif choice == "n":
         pause_print("thank you, goodbye")
         quit()
-    else:
-        pause_print("Invalid Selection..select Y or N")
-        restart_inquiry()
+    # else:
+    #     pause_print("Invalid Selection..select Y or N")
+        # restart()
 
 
 # entry point
